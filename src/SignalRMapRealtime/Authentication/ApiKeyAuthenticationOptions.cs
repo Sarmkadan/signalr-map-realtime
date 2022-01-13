187dbfb4fcaa4c302320aa02710c253eb839a364
Vladyslav Zaiets <rutova2@gmail.com>
2022-01-13 19:18:00 +0200

    cleanup.

--- src/SignalRMapRealtime/Authentication/ApiKeyAuthenticationOptions.cs
+++ src/SignalRMapRealtime/Authentication/ApiKeyAuthenticationOptions.cs
@@ -9,14 +9,20 @@ namespace SignalRMapRealtime.Authentication;
 using Microsoft.AspNetCore.Authentication;
 
 /// <summary>
-/// Options for API Key authentication.
+/// Configuration options for API Key authentication scheme.
+/// Extends <see cref="AuthenticationSchemeOptions"/> to integrate with ASP.NET Core's
+/// authentication pipeline. API keys are expected in the X-Api-Key header.
 /// </summary>
 public class ApiKeyAuthenticationOptions : AuthenticationSchemeOptions
 {
     /// <summary>
-    /// The default scheme name for API Key authentication.
+    /// The default scheme name used to register this authentication handler.
     /// </summary>
     public const string DefaultScheme = "ApiKey";
+
+    /// <summary>Gets the authentication scheme name.</summary>
     public string Scheme => DefaultScheme;
+
+    /// <summary>Authentication type identifier used in <see cref="System.Security.Claims.ClaimsIdentity"/>.</summary>
     public string AuthenticationType = DefaultScheme;
 }
