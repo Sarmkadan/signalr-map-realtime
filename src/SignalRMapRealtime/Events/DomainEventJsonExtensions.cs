8526e691ad7f58da7003df969640b123cd5ad888
Vladyslav Zaiets <rutova2@gmail.com>
2022-05-25 09:19:11 +0300

    Serialize DomainEventJson properly.

---
+++ src/SignalRMapRealtime/Events/DomainEventJsonExtensions.cs
@@ -0,0 +1,93 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Text.Json;
+
+namespace SignalRMapRealtime.Events;
+
+/// <summary>
+/// Provides System.Text.Json serialization and deserialization extensions for <see cref="DomainEvent"/> types.
+/// </summary>
+public static class DomainEventJsonExtensions
+{
+    /// <summary>
+    /// Cached JSON serialization options with camelCase naming policy.
+    /// </summary>
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        PropertyNameCaseInsensitive = true,
+    };
+
+    /// <summary>
+    /// Serializes a domain event to a JSON string.
+    /// </summary>
+    /// <param name="value">The domain event to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the domain event.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this DomainEvent value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        return JsonSerializer.Serialize(value, indented ? GetIndentedOptions() : _jsonOptions);
+    }
+
+    /// <summary>
+    /// Deserializes a domain event from a JSON string.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized domain event, or null if the JSON is empty or whitespace.</returns>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static DomainEvent? FromJson(string json)
+    {
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<DomainEvent>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a domain event from a JSON string.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized domain event if successful.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    public static bool TryFromJson(string json, out DomainEvent? value)
+    {
+        value = null;
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return false;
+        }
+
+        try
+        {
+            value = JsonSerializer.Deserialize<DomainEvent>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Gets JSON serialization options with indentation enabled.
+    /// </summary>
+    private static JsonSerializerOptions GetIndentedOptions()
+    {
+        var options = new JsonSerializerOptions(_jsonOptions)
+        {
+            WriteIndented = true,
+        };
+        return options;
+    }
+}
