188fc15b3c37a01ec5ce5e537c4a1fe027e401bc
Vladyslav Zaiets <rutova2@gmail.com>
2022-06-06 08:59:55 +0300

    tidy up ClaimsExtensionsJson json handling

---
+++ src/SignalRMapRealtime/Utilities/ClaimsExtensionsJsonExtensions.cs
@@ -0,0 +1,77 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+namespace SignalRMapRealtime.Utilities;
+
+using System.Security.Claims;
+using System.Text.Json;
+
+/// <summary>
+/// Provides System.Text.Json serialization and deserialization extensions for ClaimsPrincipal
+/// using the extension methods from ClaimsExtensions.
+/// </summary>
+public static class ClaimsExtensionsJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false
+    };
+
+    /// <summary>
+    /// Serializes a ClaimsPrincipal to a JSON string using ClaimsExtensions extension methods.
+    /// </summary>
+    /// <param name="principal">The ClaimsPrincipal to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the ClaimsPrincipal.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="principal"/> is null.</exception>
+    public static string ToJson(this ClaimsPrincipal principal, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(principal);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+            : _jsonOptions;
+
+        return JsonSerializer.Serialize(principal, options);
+    }
+
+    /// <summary>
+    /// Deserializes a ClaimsPrincipal from a JSON string.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>A ClaimsPrincipal instance, or null if the JSON is null or empty.</returns>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static ClaimsPrincipal? FromJson(string json)
+    {
+        if (string.IsNullOrEmpty(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<ClaimsPrincipal>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a ClaimsPrincipal from a JSON string.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="principal">Receives the deserialized ClaimsPrincipal instance, or null on failure.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    public static bool TryFromJson(string json, out ClaimsPrincipal? principal)
+    {
+        try
+        {
+            principal = FromJson(json);
+            return true;
+        }
+        catch (JsonException)
+        {
+            principal = null;
+            return false;
+        }
+    }
+}
