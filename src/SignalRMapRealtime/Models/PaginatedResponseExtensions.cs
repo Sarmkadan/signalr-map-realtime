6646cc67e531846b056742de20bda31f3251ae43
Vladyslav Zaiets <rutova2@gmail.com>
2022-06-27 19:31:07 +0300

    feat(models): small fix
    should be backwards compatible.

--- src/SignalRMapRealtime/Models/PaginatedResponseExtensions.cs
+++ src/SignalRMapRealtime/Models/PaginatedResponseExtensions.cs
@@ -9,6 +9,8 @@ namespace SignalRMapRealtime.Models;
 /// </summary>
 public static class PaginatedResponseExtensions
 {
+    private const int DefaultPageSize = 10;
+
     /// <summary>
     /// Creates a new paginated response with the same items but different page size.
     /// Useful for changing pagination granularity without re-querying the data source.
@@ -17,13 +19,12 @@ public static class PaginatedResponseExtensions
     /// <param name="source">The source paginated response.</param>
     /// <param name="newPageSize">The new page size to use.</param>
     /// <returns>A new paginated response with the same items but recalculated pagination metadata.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="source"/> is null.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="newPageSize"/> is less than 1.</exception>
     public static PaginatedResponse<T> WithPageSize<T>(this PaginatedResponse<T> source, int newPageSize)
     {
-        if (source == null)
-            throw new ArgumentNullException(nameof(source));
-
-        if (newPageSize < 1)
-            throw new ArgumentOutOfRangeException(nameof(newPageSize), "Page size must be positive.");
+        ArgumentNullException.ThrowIfNull(source);
+        ArgumentOutOfRangeException.ThrowIfLessThan(newPageSize, 1);
 
         var newPageNumber = Math.Min(source.PageNumber, (int)Math.Ceiling(source.TotalCount / (double)newPageSize));
 
@@ -43,13 +44,11 @@ public static class PaginatedResponseExtensions
     /// <param name="source">The source paginated response.</param>
     /// <param name="predicate">The filter predicate to apply.</param>
     /// <returns>A new paginated response with filtered items but same pagination metadata.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="source"/> or <paramref name="predicate"/> is null.</exception>
     public static PaginatedResponse<T> Where<T>(this PaginatedResponse<T> source, Func<T, bool> predicate)
     {
-        if (source == null)
-            throw new ArgumentNullException(nameof(source));
-
-        if (predicate == null)
-            throw new ArgumentNullException(nameof(predicate));
+        ArgumentNullException.ThrowIfNull(source);
+        ArgumentNullException.ThrowIfNull(predicate);
 
         var filteredItems = source.Items.Where(predicate).ToList();
 
@@ -70,15 +69,13 @@ public static class PaginatedResponseExtensions
     /// <param name="source">The source paginated response.</param>
     /// <param name="selector">The projection function.</param>
     /// <returns>A new paginated response with projected items.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="source"/> or <paramref name="selector"/> is null.</exception>
     public static PaginatedResponse<TResult> Select<T, TResult>(
         this PaginatedResponse<T> source,
         Func<T, TResult> selector)
     {
-        if (source == null)
-            throw new ArgumentNullException(nameof(source));
-
-        if (selector == null)
-            throw new ArgumentNullException(nameof(selector));
+        ArgumentNullException.ThrowIfNull(source);
+        ArgumentNullException.ThrowIfNull(selector);
 
         var projectedItems = source.Items.Select(selector).ToList();
 
@@ -97,8 +94,10 @@ public static class PaginatedResponseExtensions
     /// <typeparam name="T">The type of items in the response.</typeparam>
     /// <param name="source">The source paginated response.</param>
     /// <returns>The items on the current page.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="source"/> is null.</exception>
     public static IReadOnlyList<T> GetCurrentPage<T>(this PaginatedResponse<T> source)
     {
-        return source?.Items ?? throw new ArgumentNullException(nameof(source));
+        ArgumentNullException.ThrowIfNull(source);
+        return source.Items;
     }
 }
