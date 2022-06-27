ad91549eefcf32f393d2316d973461703e5ba394
Vladyslav Zaiets <rutova2@gmail.com>
2022-06-27 06:28:31 +0300

    feat: handy extensions for ValidationException
    no behaviour change intended.

--- src/SignalRMapRealtime/Exceptions/ValidationExceptionExtensions.cs
+++ src/SignalRMapRealtime/Exceptions/ValidationExceptionExtensions.cs
@@ -5,7 +5,8 @@ using System.Linq;
 namespace SignalRMapRealtime.Exceptions
 {
     /// <summary>
-    /// Extension methods for <see cref="ValidationException"/>.
+    /// Provides extension methods for <see cref="ValidationException"/> to facilitate
+    /// error handling and inspection of validation failures.
     /// </summary>
     public static class ValidationExceptionExtensions
     {
@@ -14,9 +15,10 @@ namespace SignalRMapRealtime.Exceptions
         /// </summary>
         /// <param name="exception">The <see cref="ValidationException"/> instance.</param>
         /// <returns><c>true</c> if there is at least one error; otherwise, <c>false</c>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is <c>null</c>.</exception>
         public static bool HasErrors(this ValidationException exception)
         {
-            if (exception == null) throw new ArgumentNullException(nameof(exception));
+            ArgumentNullException.ThrowIfNull(exception);
             return exception.Errors?.Any() ?? false;
         }
 
@@ -25,10 +27,11 @@ namespace SignalRMapRealtime.Exceptions
         /// separated by a semicolon and a space.
         /// </summary>
         /// <param name="exception">The <see cref="ValidationException"/> instance.</param>
-        /// <returns>A combined error message string.</returns>
+        /// <returns>A combined error message string, or <see cref="string.Empty"/> if no errors exist.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is <c>null</c>.</exception>
         public static string GetCombinedMessage(this ValidationException exception)
         {
-            if (exception == null) throw new ArgumentNullException(nameof(exception));
+            ArgumentNullException.ThrowIfNull(exception);
             return string.Join("; ", exception.Errors ?? Enumerable.Empty<string>());
         }
 
@@ -36,10 +39,12 @@ namespace SignalRMapRealtime.Exceptions
         /// Retrieves the distinct set of validation error messages.
         /// </summary>
         /// <param name="exception">The <see cref="ValidationException"/> instance.</param>
-        /// <returns>An <see cref="IEnumerable{String}"/> containing unique error messages.</returns>
+        /// <returns>An <see cref="IEnumerable{String}"/> containing unique error messages.
+        /// Returns an empty sequence if no errors exist.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is <c>null</c>.</exception>
         public static IEnumerable<string> GetDistinctErrors(this ValidationException exception)
         {
-            if (exception == null) throw new ArgumentNullException(nameof(exception));
+            ArgumentNullException.ThrowIfNull(exception);
             return exception.Errors?.Distinct() ?? Enumerable.Empty<string>();
         }
     }
