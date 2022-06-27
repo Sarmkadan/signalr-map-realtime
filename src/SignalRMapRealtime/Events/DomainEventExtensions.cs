04cbfa045aac54fb75c416363bcf67a29ee61974
Vladyslav Zaiets <rutova2@gmail.com>
2022-06-27 10:07:37 +0300

    A couple of DomainEvent extensions

--- src/SignalRMapRealtime/Events/DomainEventExtensions.cs
+++ src/SignalRMapRealtime/Events/DomainEventExtensions.cs
@@ -3,7 +3,7 @@
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
-// =============================================================================
+// =====================================================================
 
 using System.Text.Json;
 
@@ -13,6 +13,8 @@ namespace SignalRMapRealtime.Events;
 /// Extension methods for <see cref="DomainEvent"/> and its derived types.
 /// Provides utility methods for working with domain events.
 /// </summary>
+[System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+[System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
 public static class DomainEventExtensions
 {
     /// <summary>
@@ -22,14 +24,12 @@ public static class DomainEventExtensions
     /// </summary>
     /// <param name="domainEvent">The domain event to clone.</param>
     /// <returns>A new instance with the same property values.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="domainEvent"/> is <see langword="null"/>.</exception>
     public static DomainEvent Clone(this DomainEvent domainEvent)
     {
-        if (domainEvent is null)
-        {
-            throw new ArgumentNullException(nameof(domainEvent));
-        }
+        ArgumentNullException.ThrowIfNull(domainEvent);
 
-        DomainEvent clone = domainEvent switch
+        return domainEvent switch
         {
             LocationUpdatedEvent locationEvent => new LocationUpdatedEvent
             {
@@ -92,8 +92,6 @@ public static class DomainEventExtensions
 
             _ => throw new InvalidOperationException($"Unknown DomainEvent type: {domainEvent.GetType().FullName}")
         };
-
-        return clone;
     }
 
     /// <summary>
@@ -103,12 +101,10 @@ public static class DomainEventExtensions
     /// <param name="domainEvent">The domain event to serialize.</param>
     /// <param name="options">Optional JSON serialization options.</param>
     /// <returns>JSON string representation of the event.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="domainEvent"/> is <see langword="null"/>.</exception>
     public static string ToJson(this DomainEvent domainEvent, JsonSerializerOptions? options = null)
     {
-        if (domainEvent is null)
-        {
-            throw new ArgumentNullException(nameof(domainEvent));
-        }
+        ArgumentNullException.ThrowIfNull(domainEvent);
 
         options ??= new JsonSerializerOptions
         {
@@ -125,8 +121,11 @@ public static class DomainEventExtensions
     /// </summary>
     /// <param name="domainEvent">The domain event to check.</param>
     /// <returns>True if the event is a LocationUpdatedEvent; otherwise false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="domainEvent"/> is <see langword="null"/>.</exception>
     public static bool IsLocationUpdate(this DomainEvent domainEvent)
     {
+        ArgumentNullException.ThrowIfNull(domainEvent);
+
         return domainEvent is LocationUpdatedEvent;
     }
 
@@ -136,19 +135,25 @@ public static class DomainEventExtensions
     /// </summary>
     /// <param name="domainEvent">The domain event to check.</param>
     /// <returns>True if the event is a VehicleStatusChangedEvent; otherwise false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="domainEvent"/> is <see langword="null"/>.</exception>
     public static bool IsStatusChange(this DomainEvent domainEvent)
     {
+        ArgumentNullException.ThrowIfNull(domainEvent);
+
         return domainEvent is VehicleStatusChangedEvent;
     }
 
     /// <summary>
     /// Gets the vehicle identifier from the domain event.
-    /// Returns Guid.Empty if the event doesn't contain vehicle information.
+    /// Returns <see cref="Guid"/>.<see cref="Guid.Empty"/> if the event doesn't contain vehicle information.
     /// </summary>
     /// <param name="domainEvent">The domain event.</param>
-    /// <returns>The vehicle ID or Guid.Empty if not applicable.</returns>
+    /// <returns>The vehicle ID or <see cref="Guid"/>.<see cref="Guid.Empty"/> if not applicable.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="domainEvent"/> is <see langword="null"/>.</exception>
     public static Guid GetVehicleId(this DomainEvent domainEvent)
     {
+        ArgumentNullException.ThrowIfNull(domainEvent);
+
         return domainEvent switch
         {
             LocationUpdatedEvent locationEvent => locationEvent.VehicleId,
@@ -165,12 +170,10 @@ public static class DomainEventExtensions
     /// </summary>
     /// <param name="domainEvent">The domain event.</param>
     /// <returns>Human-readable description of the event.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="domainEvent"/> is <see langword="null"/>.</exception>
     public static string GetDescription(this DomainEvent domainEvent)
     {
-        if (domainEvent is null)
-        {
-            throw new ArgumentNullException(nameof(domainEvent));
-        }
+        ArgumentNullException.ThrowIfNull(domainEvent);
 
         return domainEvent switch
         {
