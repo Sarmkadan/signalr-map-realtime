b8e54d85294ab89f3b7c921f11be7f9037570715
Vladyslav Zaiets <rutova2@gmail.com>
2022-07-08 19:54:51 +0300

    feat(controllers): a couple of AssetController extensions

--- src/SignalRMapRealtime/Controllers/AssetControllerExtensions.cs
+++ src/SignalRMapRealtime/Controllers/AssetControllerExtensions.cs
@@ -3,129 +3,313 @@
 namespace SignalRMapRealtime.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
+using SignalRMapRealtime.Domain.Enums;
 using SignalRMapRealtime.DTOs;
 using SignalRMapRealtime.Models;
+using SignalRMapRealtime.Utilities;
 
 /// <summary>
-/// Extension methods for AssetController providing additional asset management functionality.
+/// Extension methods for <see cref="AssetController"/> providing additional asset management functionality.
 /// </summary>
 public static class AssetControllerExtensions
 {
     /// <summary>
     /// Gets assets filtered by type with pagination.
     /// </summary>
-    /// <param name="controller">The AssetController instance</param>
-    /// <param name="assetType">The type of assets to filter by</param>
-    /// <param name="pageNumber">Page number for pagination</param>
-    /// <param name="pageSize">Number of items per page</param>
-    /// <returns>Action result with filtered assets</returns>
+    /// <param name="controller">The <see cref="AssetController"/> instance.</param>
+    /// <param name="assetType">The type of assets to filter by.</param>
+    /// <param name="pageNumber">Page number for pagination.</param>
+    /// <param name="pageSize">Number of items per page.</param>
+    /// <returns>Action result with filtered assets.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="controller"/> is <see langword="null"/>.</exception>
     public static async Task<IActionResult> GetAssetsByType(this AssetController controller,
         [FromQuery] string assetType,
         [FromQuery] int pageNumber = 1,
         [FromQuery] int pageSize = 20)
     {
+        ArgumentNullException.ThrowIfNull(controller);
+
         if (string.IsNullOrWhiteSpace(assetType))
         {
             return controller.BadRequest(ErrorResponse.ValidationError(
-                new Dictionary<string, string[]> { { "assetType", new[] { "Asset type is required" } } },
+                new Dictionary<string, string[]> { { "assetType", ["Asset type is required"] } },
                 "Asset type parameter is required",
                 controller.HttpContext.TraceIdentifier));
         }
 
-        return await controller.GetAssets(pageNumber, pageSize);
+        if (!Enum.TryParse<AssetType>(assetType, true, out var parsedAssetType))
+        {
+            return controller.BadRequest(ErrorResponse.ValidationError(
+                new Dictionary<string, string[]> { { "assetType", ["Invalid asset type value"] } },
+                "Invalid asset type specified",
+                controller.HttpContext.TraceIdentifier));
+        }
+
+        try
+        {
+            var (validPageNumber, validPageSize) = PaginationExtensions.NormalizePaginationParameters(pageNumber, pageSize, 100);
+            var allAssetsResult = await controller.GetAssets(validPageNumber, validPageSize);
+
+            if (allAssetsResult is not OkObjectResult okResult)
+            {
+                return allAssetsResult;
+            }
+
+            var response = okResult.Value as ApiResponse<PaginatedResponse<AssetDto>>;
+            var assets = response?.Data?.Items ?? new List<AssetDto>();
+
+            var filteredAssets = assets
+                .Where(a => a.AssetType == parsedAssetType)
+                .ToList();
+
+            var paginatedResponse = PaginatedResponse<AssetDto>.FromList(
+                filteredAssets,
+                validPageNumber,
+                validPageSize);
+
+            var resultResponse = ApiResponse<PaginatedResponse<AssetDto>>.SuccessResponse(
+                paginatedResponse,
+                "Assets filtered by type retrieved successfully",
+                200,
+                controller.HttpContext.TraceIdentifier);
+
+            return controller.Ok(resultResponse);
+        }
+        catch
+        {
+            return controller.BadRequest(ErrorResponse.ServerError(
+                "Failed to retrieve assets by type",
+                controller.HttpContext.TraceIdentifier));
+        }
     }
 
     /// <summary>
     /// Gets assets filtered by condition with pagination.
     /// </summary>
-    /// <param name="controller">The AssetController instance</param>
-    /// <param name="condition">The condition to filter assets by</param>
-    /// <param name="pageNumber">Page number for pagination</param>
-    /// <param name="pageSize">Number of items per page</param>
-    /// <returns>Action result with filtered assets</returns>
+    /// <param name="controller">The <see cref="AssetController"/> instance.</param>
+    /// <param name="condition">The condition to filter assets by.</param>
+    /// <param name="pageNumber">Page number for pagination.</param>
+    /// <param name="pageSize">Number of items per page.</param>
+    /// <returns>Action result with filtered assets.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="controller"/> is <see langword="null"/>.</exception>
     public static async Task<IActionResult> GetAssetsByCondition(this AssetController controller,
         [FromQuery] string condition,
         [FromQuery] int pageNumber = 1,
         [FromQuery] int pageSize = 20)
     {
-        return await controller.GetAssets(pageNumber, pageSize);
+        ArgumentNullException.ThrowIfNull(controller);
+
+        if (string.IsNullOrWhiteSpace(condition))
+        {
+            return controller.BadRequest(ErrorResponse.ValidationError(
+                new Dictionary<string, string[]> { { "condition", ["Condition parameter is required"] } },
+                "Condition parameter is required",
+                controller.HttpContext.TraceIdentifier));
+        }
+
+        try
+        {
+            var (validPageNumber, validPageSize) = PaginationExtensions.NormalizePaginationParameters(pageNumber, pageSize, 100);
+            var allAssetsResult = await controller.GetAssets(validPageNumber, validPageSize);
+
+            if (allAssetsResult is not OkObjectResult okResult)
+            {
+                return allAssetsResult;
+            }
+
+            var response = okResult.Value as ApiResponse<PaginatedResponse<AssetDto>>;
+            var assets = response?.Data?.Items ?? new List<AssetDto>();
+
+            var filteredAssets = assets
+                .Where(a => !string.IsNullOrEmpty(a.Condition) && a.Condition.Equals(condition, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+
+            var paginatedResponse = PaginatedResponse<AssetDto>.FromList(
+                filteredAssets,
+                validPageNumber,
+                validPageSize);
+
+            var resultResponse = ApiResponse<PaginatedResponse<AssetDto>>.SuccessResponse(
+                paginatedResponse,
+                "Assets filtered by condition retrieved successfully",
+                200,
+                controller.HttpContext.TraceIdentifier);
+
+            return controller.Ok(resultResponse);
+        }
+        catch
+        {
+            return controller.BadRequest(ErrorResponse.ServerError(
+                "Failed to retrieve assets by condition",
+                controller.HttpContext.TraceIdentifier));
+        }
     }
 
     /// <summary>
     /// Gets a summary of asset statistics including counts by type and condition.
     /// </summary>
-    /// <param name="controller">The AssetController instance</param>
-    /// <returns>Action result with asset statistics</returns>
+    /// <param name="controller">The <see cref="AssetController"/> instance.</param>
+    /// <returns>Action result with asset statistics.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="controller"/> is <see langword="null"/>.</exception>
     public static async Task<IActionResult> GetAssetStatistics(this AssetController controller)
     {
-        var allAssetsResult = await controller.GetAssets(1, 1000);
-        if (allAssetsResult is not OkObjectResult okResult)
+        ArgumentNullException.ThrowIfNull(controller);
+
+        try
         {
-            return allAssetsResult;
+            var allAssetsResult = await controller.GetAssets(1, 1000);
+
+            if (allAssetsResult is not OkObjectResult okResult)
+            {
+                return allAssetsResult;
+            }
+
+            var response = okResult.Value as ApiResponse<PaginatedResponse<AssetDto>>;
+            var assets = response?.Data?.Items ?? new List<AssetDto>();
+
+            var statistics = new AssetStatisticsDto
+            {
+                TotalAssets = assets.Count,
+                AssetsByType = assets
+                    .GroupBy(a => a.AssetType.ToString() ?? "Unknown")
+                    .ToDictionary(g => g.Key, g => g.Count()),
+                AssetsByCondition = assets
+                    .Where(a => !string.IsNullOrEmpty(a.Condition))
+                    .GroupBy(a => a.Condition!)
+                    .ToDictionary(g => g.Key, g => g.Count()),
+                LastUpdated = assets.Any() ? assets.Max(a => a.UpdatedAt) : DateTime.UtcNow
+            };
+
+            var resultResponse = ApiResponse<AssetStatisticsDto>.SuccessResponse(
+                statistics,
+                "Asset statistics retrieved successfully",
+                200,
+                controller.HttpContext.TraceIdentifier);
+
+            return controller.Ok(resultResponse);
         }
-
-        var response = okResult.Value as ApiResponse<PaginatedResponse<AssetDto>>;
-        var assets = response?.Data?.Items ?? new List<AssetDto>();
-
-        var statistics = new AssetStatisticsDto
+        catch
         {
-            TotalAssets = assets.Count,
-            AssetsByType = assets
-                .GroupBy(a => a.AssetType.ToString() ?? "Unknown")
-                .ToDictionary(g => g.Key, g => g.Count()),
-            AssetsByCondition = assets
-                .Where(a => !string.IsNullOrEmpty(a.Condition))
-                .GroupBy(a => a.Condition!)
-                .ToDictionary(g => g.Key, g => g.Count()),
-            LastUpdated = assets.Any() ? assets.Max(a => a.UpdatedAt) : DateTime.UtcNow
-        };
-
-        var resultResponse = ApiResponse<AssetStatisticsDto>.SuccessResponse(
-            statistics,
-            "Asset statistics retrieved successfully",
-            200,
-            controller.HttpContext.TraceIdentifier);
-
-        return controller.Ok(resultResponse);
+            return controller.BadRequest(ErrorResponse.ServerError(
+                "Failed to retrieve asset statistics",
+                controller.HttpContext.TraceIdentifier));
+        }
     }
 
     /// <summary>
     /// Gets assets created within a specific time range.
     /// </summary>
-    /// <param name="controller">The AssetController instance</param>
-    /// <param name="startDate">Start date of the range</param>
-    /// <param name="endDate">End date of the range</param>
-    /// <param name="pageNumber">Page number for pagination</param>
-    /// <param name="pageSize">Number of items per page</param>
-    /// <returns>Action result with time-filtered assets</returns>
+    /// <param name="controller">The <see cref="AssetController"/> instance.</param>
+    /// <param name="startDate">Start date of the range (inclusive).</param>
+    /// <param name="endDate">End date of the range (inclusive).</param>
+    /// <param name="pageNumber">Page number for pagination.</param>
+    /// <param name="pageSize">Number of items per page.</param>
+    /// <returns>Action result with time-filtered assets.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="controller"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException"><paramref name="startDate"/> is after <paramref name="endDate"/>.</exception>
     public static async Task<IActionResult> GetAssetsByDateRange(this AssetController controller,
         [FromQuery] DateTime startDate,
         [FromQuery] DateTime endDate,
         [FromQuery] int pageNumber = 1,
         [FromQuery] int pageSize = 20)
     {
+        ArgumentNullException.ThrowIfNull(controller);
+
         if (startDate > endDate)
         {
             return controller.BadRequest(ErrorResponse.ValidationError(
-                new Dictionary<string, string[]> { { "dateRange", new[] { "Start date must be before end date" } } },
+                new Dictionary<string, string[]> { { "dateRange", ["Start date must be before end date"] } },
                 "Invalid date range",
                 controller.HttpContext.TraceIdentifier));
         }
 
-        return await controller.GetAssets(pageNumber, pageSize);
+        try
+        {
+            var (validPageNumber, validPageSize) = PaginationExtensions.NormalizePaginationParameters(pageNumber, pageSize, 100);
+            var allAssetsResult = await controller.GetAssets(validPageNumber, validPageSize);
+
+            if (allAssetsResult is not OkObjectResult okResult)
+            {
+                return allAssetsResult;
+            }
+
+            var response = okResult.Value as ApiResponse<PaginatedResponse<AssetDto>>;
+            var assets = response?.Data?.Items ?? new List<AssetDto>();
+
+            var filteredAssets = assets
+                .Where(a => a.CreatedAt >= startDate && a.CreatedAt <= endDate)
+                .ToList();
+
+            var paginatedResponse = PaginatedResponse<AssetDto>.FromList(
+                filteredAssets,
+                validPageNumber,
+                validPageSize);
+
+            var resultResponse = ApiResponse<PaginatedResponse<AssetDto>>.SuccessResponse(
+                paginatedResponse,
+                "Assets filtered by date range retrieved successfully",
+                200,
+                controller.HttpContext.TraceIdentifier);
+
+            return controller.Ok(resultResponse);
+        }
+        catch
+        {
+            return controller.BadRequest(ErrorResponse.ServerError(
+                "Failed to retrieve assets by date range",
+                controller.HttpContext.TraceIdentifier));
+        }
     }
 
     /// <summary>
     /// Gets assets sorted by creation date in ascending order.
     /// </summary>
-    /// <param name="controller">The AssetController instance</param>
-    /// <param name="pageNumber">Page number for pagination</param>
-    /// <param name="pageSize">Number of items per page</param>
-    /// <returns>Action result with sorted assets</returns>
+    /// <param name="controller">The <see cref="AssetController"/> instance.</param>
+    /// <param name="pageNumber">Page number for pagination.</param>
+    /// <param name="pageSize">Number of items per page.</param>
+    /// <returns>Action result with sorted assets.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="controller"/> is <see langword="null"/>.</exception>
     public static async Task<IActionResult> GetAssetsSortedByDate(this AssetController controller,
         [FromQuery] int pageNumber = 1,
         [FromQuery] int pageSize = 20)
     {
-        return await controller.GetAssets(pageNumber, pageSize);
+        ArgumentNullException.ThrowIfNull(controller);
+
+        try
+        {
+            var (validPageNumber, validPageSize) = PaginationExtensions.NormalizePaginationParameters(pageNumber, pageSize, 100);
+            var allAssetsResult = await controller.GetAssets(validPageNumber, validPageSize);
+
+            if (allAssetsResult is not OkObjectResult okResult)
+            {
+                return allAssetsResult;
+            }
+
+            var response = okResult.Value as ApiResponse<PaginatedResponse<AssetDto>>;
+            var assets = response?.Data?.Items ?? new List<AssetDto>();
+
+            var sortedAssets = assets
+                .OrderBy(a => a.CreatedAt)
+                .ToList();
+
+            var paginatedResponse = PaginatedResponse<AssetDto>.FromList(
+                sortedAssets,
+                validPageNumber,
+                validPageSize);
+
+            var resultResponse = ApiResponse<PaginatedResponse<AssetDto>>.SuccessResponse(
+                paginatedResponse,
+                "Assets sorted by creation date retrieved successfully",
+                200,
+                controller.HttpContext.TraceIdentifier);
+
+            return controller.Ok(resultResponse);
+        }
+        catch
+        {
+            return controller.BadRequest(ErrorResponse.ServerError(
+                "Failed to retrieve assets sorted by date",
+                controller.HttpContext.TraceIdentifier));
+        }
     }
 }
