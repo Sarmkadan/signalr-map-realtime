0ded86525855a0be9ccd62ed853c840c2e7c8ffe
Vladyslav Zaiets <rutova2@gmail.com>
2022-07-11 08:25:52 +0300

    Extension methods on LocationController.
    should be backwards compatible.

--- src/SignalRMapRealtime/Controllers/LocationControllerExtensions.cs
+++ src/SignalRMapRealtime/Controllers/LocationControllerExtensions.cs
@@ -3,17 +3,17 @@
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
-// =============================================================================
+// =====================================================================
 
 namespace SignalRMapRealtime.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
 using SignalRMapRealtime.DTOs;
 using SignalRMapRealtime.Models;
+using SignalRMapRealtime.Utilities;
 
 /// <summary>
-/// Extension methods for LocationController providing additional functionality
-/// such as location filtering and retrieval capabilities.
+/// Extension methods for <see cref="LocationController"/> providing additional location filtering and retrieval capabilities.
 /// </summary>
 public static class LocationControllerExtensions
 {
@@ -21,10 +21,12 @@ public static class LocationControllerExtensions
     /// Gets locations filtered by vehicle ID and date range.
     /// Returns 200 OK with filtered locations.
     /// </summary>
-    /// <param name="controller">The LocationController instance</param>
-    /// <param name="vehicleId">Vehicle ID to filter by</param>
-    /// <param name="startDate">Optional start date for filtering</param>
-    /// <param name="endDate">Optional end date for filtering</param>
+    /// <param name="controller">The <see cref="LocationController"/> instance.</param>
+    /// <param name="vehicleId">Vehicle ID to filter by.</param>
+    /// <param name="startDate">Optional start date for filtering (inclusive).</param>
+    /// <param name="endDate">Optional end date for filtering (inclusive).</param>
+    /// <returns>Action result with filtered locations.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="controller"/> is <see langword="null"/>.</exception>
     [HttpGet("vehicle/{vehicleId}")]
     public static async Task<IActionResult> GetLocationsByVehicleAndDateRange(
         this LocationController controller,
@@ -32,41 +34,63 @@ public static class LocationControllerExtensions
         [FromQuery] DateTime? startDate = null,
         [FromQuery] DateTime? endDate = null)
     {
+        ArgumentNullException.ThrowIfNull(controller);
+
+        if (startDate.HasValue && endDate.HasValue && startDate.Value > endDate.Value)
+        {
+            return controller.BadRequest(ErrorResponse.ValidationError(
+                new Dictionary<string, string[]> {
+                    { "dateRange", new[] { "Start date must be before or equal to end date" } }
+                },
+                "Invalid date range",
+                controller.HttpContext.TraceIdentifier));
+        }
+
         try
         {
-            var result = await controller.GetLocations(1, 100, vehicleId);
+            // Get all locations for the vehicle using the controller's existing method
+            var result = await controller.GetLocations(1, 1000, vehicleId);
 
-            if (result is OkObjectResult okResult && okResult.Value is ApiResponse<PaginatedResponse<LocationDto>> apiResponse && apiResponse.Data?.Items != null)
+            if (result is not OkObjectResult okResult)
             {
-                var filteredLocations = apiResponse.Data.Items.AsEnumerable();
-
-                if (startDate.HasValue || endDate.HasValue)
-                {
-                    filteredLocations = filteredLocations.Where(l =>
-                    {
-                        var locationDate = l.Timestamp.Date;
-                        return (!startDate.HasValue || locationDate >= startDate.Value.Date) &&
-                               (!endDate.HasValue || locationDate <= endDate.Value.Date);
-                    });
-                }
-
-                var filteredResponse = PaginatedResponse<LocationDto>.FromList(
-                    filteredLocations.ToList(),
-                    1,
-                    filteredLocations.Count());
-
-                var response = ApiResponse<PaginatedResponse<LocationDto>>.SuccessResponse(
-                    filteredResponse,
-                    "Filtered locations retrieved successfully",
-                    200,
-                    controller.HttpContext.TraceIdentifier);
-
-                return controller.Ok(response);
+                return result;
             }
 
-            return controller.BadRequest(ErrorResponse.ServerError("Failed to retrieve locations", controller.HttpContext.TraceIdentifier));
+            var apiResponse = okResult.Value as ApiResponse<PaginatedResponse<LocationDto>>;
+
+            if (apiResponse?.Data?.Items == null)
+            {
+                return controller.BadRequest(ErrorResponse.ServerError("Failed to retrieve locations", controller.HttpContext.TraceIdentifier));
+            }
+
+            var filteredLocations = apiResponse.Data.Items.AsEnumerable();
+
+            // Apply date filtering if specified
+            if (startDate.HasValue || endDate.HasValue)
+            {
+                filteredLocations = filteredLocations.Where(l =>
+                {
+                    var locationDate = l.Timestamp.Date;
+                    return (!startDate.HasValue || locationDate >= startDate.Value.Date) &&
+                           (!endDate.HasValue || locationDate <= endDate.Value.Date);
+                });
+            }
+
+            var filteredList = filteredLocations.ToList();
+            var filteredResponse = PaginatedResponse<LocationDto>.FromList(
+                filteredList,
+                1,
+                filteredList.Count);
+
+            var response = ApiResponse<PaginatedResponse<LocationDto>>.SuccessResponse(
+                filteredResponse,
+                "Filtered locations retrieved successfully",
+                200,
+                controller.HttpContext.TraceIdentifier);
+
+            return controller.Ok(response);
         }
-        catch (Exception ex)
+        catch
         {
             return controller.BadRequest(ErrorResponse.ServerError("Failed to retrieve vehicle locations", controller.HttpContext.TraceIdentifier));
         }
@@ -76,36 +100,48 @@ public static class LocationControllerExtensions
     /// Gets the most recent location for a specific vehicle.
     /// Returns 200 OK with the latest location or 404 if not found.
     /// </summary>
-    /// <param name="controller">The LocationController instance</param>
-    /// <param name="vehicleId">Vehicle ID to get latest location for</param>
+    /// <param name="controller">The <see cref="LocationController"/> instance.</param>
+    /// <param name="vehicleId">Vehicle ID to get latest location for.</param>
+    /// <returns>Action result with the latest location or 404 if not found.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="controller"/> is <see langword="null"/>.</exception>
     [HttpGet("vehicle/{vehicleId}/latest")]
     public static async Task<IActionResult> GetLatestLocationByVehicle(
         this LocationController controller,
         [FromRoute] int vehicleId)
     {
+        ArgumentNullException.ThrowIfNull(controller);
+
         try
         {
-            // Get all locations for the vehicle, sorted by timestamp descending
+            // Get locations for the vehicle (limit to 1 page with 1 item to get the most recent)
             var result = await controller.GetLocations(1, 1, vehicleId);
 
-            if (result is OkObjectResult okResult && okResult.Value is ApiResponse<PaginatedResponse<LocationDto>> apiResponse && apiResponse.Data?.Items != null && apiResponse.Data.Items.Any())
+            if (result is not OkObjectResult okResult)
             {
-                var latestLocation = apiResponse.Data.Items.OrderByDescending(l => l.Timestamp).First();
-
-                var response = ApiResponse<LocationDto>.SuccessResponse(
-                    latestLocation,
-                    "Latest location retrieved successfully",
-                    200,
-                    controller.HttpContext.TraceIdentifier);
-
-                return controller.Ok(response);
+                return result;
             }
 
-            return controller.NotFound(ErrorResponse.NotFoundError(
-                $"No locations found for vehicle ID {vehicleId}",
-                controller.HttpContext.TraceIdentifier));
+            var apiResponse = okResult.Value as ApiResponse<PaginatedResponse<LocationDto>>;
+
+            if (apiResponse?.Data?.Items == null || !apiResponse.Data.Items.Any())
+            {
+                return controller.NotFound(ErrorResponse.NotFoundError(
+                    $"No locations found for vehicle ID {vehicleId}",
+                    controller.HttpContext.TraceIdentifier));
+            }
+
+            // Get the latest location by timestamp
+            var latestLocation = apiResponse.Data.Items.MaxBy(l => l.Timestamp);
+
+            var response = ApiResponse<LocationDto>.SuccessResponse(
+                latestLocation!,
+                "Latest location retrieved successfully",
+                200,
+                controller.HttpContext.TraceIdentifier);
+
+            return controller.Ok(response);
         }
-        catch (Exception ex)
+        catch
         {
             return controller.BadRequest(ErrorResponse.ServerError("Failed to retrieve latest location", controller.HttpContext.TraceIdentifier));
         }
@@ -115,13 +151,17 @@ public static class LocationControllerExtensions
     /// Gets locations by multiple IDs in a single request.
     /// Returns 200 OK with all found locations.
     /// </summary>
-    /// <param name="controller">The LocationController instance</param>
-    /// <param name="ids">Array of location IDs to retrieve</param>
+    /// <param name="controller">The <see cref="LocationController"/> instance.</param>
+    /// <param name="ids">Array of location IDs to retrieve.</param>
+    /// <returns>Action result with found locations and any not found IDs.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="controller"/> is <see langword="null"/>.</exception>
     [HttpGet("batch")]
     public static async Task<IActionResult> GetLocationsByIds(
         this LocationController controller,
         [FromQuery] Guid[] ids)
     {
+        ArgumentNullException.ThrowIfNull(controller);
+
         if (ids == null || ids.Length == 0)
         {
             return controller.BadRequest(ErrorResponse.ValidationError(
@@ -140,6 +180,7 @@ public static class LocationControllerExtensions
             foreach (var id in ids)
             {
                 var result = await controller.GetLocationById(id);
+
                 if (result is OkObjectResult okResult && okResult.Value is ApiResponse<LocationDto> apiResponse && apiResponse.Data != null)
                 {
                     results.Add(apiResponse.Data);
@@ -160,7 +201,7 @@ public static class LocationControllerExtensions
 
             return controller.Ok(response);
         }
-        catch (Exception ex)
+        catch
         {
             return controller.BadRequest(ErrorResponse.ServerError("Failed to retrieve batch locations", controller.HttpContext.TraceIdentifier));
         }
