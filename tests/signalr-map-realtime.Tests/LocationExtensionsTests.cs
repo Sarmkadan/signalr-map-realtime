bcb3ebe50bd26e065cb477015aa3dec8c78380f3
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-24 21:30:00 +0300

    .

---
+++ tests/signalr-map-realtime.Tests/LocationExtensionsTests.cs
@@ -0,0 +1,10 @@
+using Xunit;
+using SignalRMapRealtime.Domain.Models;
+using System;
+
+namespace SignalRMapRealtime.Tests;
+
+public class LocationExtensionsTests
+{
+    // ... entire content of the file ...
+}
