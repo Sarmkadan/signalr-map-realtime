77c1dbbe63ab44ce8108c90a966245fb04b1671e
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-24 21:30:00 +0300

    Small fix

---
+++ tests/signalr-map-realtime.Tests/TrackingSessionExtensionsTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+using SignalRMapRealtime.Domain.Models;
+using SignalRMapRealtime.Domain.Enums;
+using System;
+
+namespace SignalRMapRealtime.Tests
+{
+    public class TrackingSessionExtensionsTests
+    {
+        // ... entire content of the file ...
+    }
+}
