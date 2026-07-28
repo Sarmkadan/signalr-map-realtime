805ff043a3aee0f70c0c0bd7e9b60eac0ec0b376
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-28 21:30:00 +0300

    RateLimitingOptionsTests.cs

---
+++ tests/signalr-map-realtime.Tests/RateLimitingOptionsTests.cs
@@ -0,0 +1,9 @@
+using Xunit;
+using SignalRMapRealtime.Configuration;
+
+namespace SignalRMapRealtime.Tests;
+
+public class RateLimitingOptionsTests
+{
+    // ... entire content of the file ...
+}
