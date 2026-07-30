8d236f7cf8685ebf170d873ebc71172ea6036616
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 10:15:05 +0300

    Fix the build

---
+++ tests/signalr-map-realtime.Tests/VehicleDtoExtensionsTests.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using SignalRMapRealtime.DTOs;
+using SignalRMapRealtime.Tests;
+using Xunit;
+
+namespace SignalRMapRealtime.Tests
+{
+    public class VehicleDtoExtensionsTests
+    {
+        [Fact]
+        public async Task TestIsOnline()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestIsInStatus()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestGetStatusString()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestGetLocationDetails()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestGetInfoString()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestRequiresAttention()
+        {
+            // Test implementation here
+        }
+    }
+}
